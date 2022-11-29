b35e8f1024e804e1cb5d0ef2791e0ce5fb2b2b18
MarkZaydman <84101694+MarkZaydman@users.noreply.github.com>
2022-11-29 15:53:59 -0600

    Add type hinting to transforms

--- transforms.py
+++ transforms.py
@@ -14,41 +14,47 @@ Purpose: Helper functions for generating transforms used in PC clonality index
 import numpy as np
 import pandas as pd
 import scipy.linalg
+from typing import Callable
 
-def log_transform(INPUT,inverse=False):
-    OUTPUT=np.copy(INPUT)
-    if inverse:
-        OUTPUT=np.exp(INPUT)
-    else:
-        OUTPUT=np.log(INPUT)
-    return(OUTPUT)
+def log_transform(INPUT: np.array, inverse: bool = False) -> np.array:
+	"""Returns log transformed or inverse log transformed version of input np array"""
+	OUTPUT=np.copy(INPUT)
+	if inverse:
+	OUTPUT=np.exp(INPUT)
+	else:
+	OUTPUT=np.log(INPUT)
+	return(OUTPUT)
 
-def make_ztransform(X):
-    def z_transform(INPUT,inverse=False):
-        OUTPUT=np.copy(INPUT)
-        if inverse:
-            for col in range(INPUT.shape[1]):
-                OUTPUT[:,col]=INPUT[:,col]*np.std(X[:,col])+np.mean(X[:,col])
-        else:
-            for col in range(INPUT.shape[1]):
-                OUTPUT[:,col]=(INPUT[:,col]-np.mean(X[:,col]))/np.std(X[:,col])
-        return(OUTPUT)
-    return(z_transform)
+def make_ztransform(X: np.array)->Callable[[np.array,bool],np.array]:
+	"""Returns z transform function for data model X"""
+	def z_transform(INPUT: np.array,inverse: bool =False) -> np.array:
+		"""Returns the z transformed or inverse z transformed version of the input np array"""
+		OUTPUT=np.copy(INPUT)
+		if inverse:
+		    for col in range(INPUT.shape[1]):
+			OUTPUT[:,col]=INPUT[:,col]*np.std(X[:,col])+np.mean(X[:,col])
+		else:
+		    for col in range(INPUT.shape[1]):
+			OUTPUT[:,col]=(INPUT[:,col]-np.mean(X[:,col]))/np.std(X[:,col])
+		return(OUTPUT)
+	return(z_transform)
 
-def make_pctransform(X):
-    U,S,Vh=scipy.linalg.svd(X,full_matrices=False)
-    def pc_transform(INPUT,inverse=False, return_decomposition=False):
-        if inverse:
-            OUTPUT=INPUT@Vh
-        else:
-            OUTPUT=np.copy(INPUT)
-            for row in range(INPUT.shape[0]):
-                OUTPUT[row,:]=INPUT[row,:]@Vh.T
-        if return_decomposition:
-            return(OUTPUT, U, S, Vh)
-        else:
-            return(OUTPUT)
-    return(pc_transform)
+def make_pctransform(X: np.array)->Callable[[np.array,bool],np.array]:
+	"""Returns PC transform function for data model X"""
+	U,S,Vh=scipy.linalg.svd(X,full_matrices=False)
+	def pc_transform(INPUT: np.array,inverse=False, return_decomposition=False) -> np.array:
+		"""Returns the PC transformed or inverse PC transformed version of the input np array"""
+		if inverse:
+			OUTPUT=INPUT@Vh
+		else:
+			OUTPUT=np.copy(INPUT)
+			for row in range(INPUT.shape[0]):
+				OUTPUT[row,:]=INPUT[row,:]@Vh.T
+		if return_decomposition:
+			return(OUTPUT, U, S, Vh)
+		else:
+			return(OUTPUT)
+	return(pc_transform)
 
 def find_RI(X,lb,ub):
     RI=[np.percentile(X,lb),
