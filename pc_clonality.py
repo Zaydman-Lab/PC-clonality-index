5979f7fbf6094e859475569c6cbb5b759cb47854
markzaydman <zaydmanm@wustl.edu>
2022-11-30 12:52:49 -0600

    Update type hinting and doc strings

--- pc_clonality.py
+++ pc_clonality.py
@@ -1,23 +1,35 @@
 """
 pc_clonality.py
-Created by Vahid Azimi
+Created by Vahid Azimi and Mark Zaydman
 11/29/22
 
 Purpose: main point of entry into PC-clonality-index package
 
-This script will execute one of the three cases described in the .README based on the user input
+This script will enable the user to derive a pc2-based equation and interval using either a user defined or
+the WashU reference nonmg cohort, to evaluate the sensitivity and specificity of the pc2-based
+interval for mg diagnosis using either a user defined or the Wash U mg cohorts, and to apply 
+the pc2 based interval a new set of cases. The output behavior is controlled by a set of 
+optional command line arguments.
 
 Functions:
+	parse_args()->Tuple[str]:
+		'''Returns parsed arguments'''
+	df2array(df: pd.DataFrame)->np.array:
+		'''Returns np array of kappa and labmda values derived from input dataframe'''
+	derive_interval(nonmg: pd.DataFrame,lb: float,ub: float)->Tuple[np.array,list[float,float],Callable[[np.array,bool],np.array],Callable[[np.array,bool],np.array]]:
+		'''Derive PC2-based equation and refence interval using WU nonmg cohort or user input nonmg cohort'''
+	evaluate_interval(nonmg: pd.DataFrame, mg: pd.DataFrame, lb: float, ub: float)->None:
+		'''Evaluate PC2-based interval for MG diagnosis'''
+	def apply_interval(nonmg: pd.DataFrame,cases: pd.DataFrame, lb: float, ub: float):
+		'''Apply PC2-based interval to new patient data'''
+	main(nonmg_fpath: str = None,mg_fpath: str = None,cases_fpath: str = None,lb: float = 2.5,ub: float = 97.5)->None:
+		'''Generates output per user specified options'''
 
-
-generate_embedding(nonmg_path,lb,ub,output_path):
-    '''Returns tranform object and embeddings'''
 """
 
 #%% imports
 from optparse import OptionParser
 import pandas as pd 
-import matplotlib.pyplot as plt
 import pickle
 from typing import Tuple, Callable
 import visualize
@@ -25,8 +37,6 @@ import transforms
 import evaluate
 import numpy as np
 
-
-#%% helper functions
 def parse_args()->Tuple[str]:
 	"""Returns parsed arguments"""
 	parser = OptionParser()
@@ -64,7 +74,7 @@ def df2array(df: pd.DataFrame)->np.array:
 	return(X)
 
 def derive_interval(nonmg: pd.DataFrame,lb: float,ub: float)->Tuple[np.array,list[float,float],Callable[[np.array,bool],np.array],Callable[[np.array,bool],np.array]]:
-	"""Derive PC2-based refence interval using WU nonmg cohort or user input nonmg cohort"""
+	"""Derive PC2-based equation and refence interval using WU nonmg cohort or user input nonmg cohort"""
 	X_nonmg = df2array(nonmg)
 	L_nonmg = transforms.log_transform(X_nonmg) #apply log transform to X_nonmg
 	z_transform = transforms.make_ztransform(L_nonmg) #compute z transform of L_nonmg
@@ -93,13 +103,13 @@ def derive_interval(nonmg: pd.DataFrame,lb: float,ub: float)->Tuple[np.array,lis
 		f.write(f"\t%s %%ile = %.2f" % (ub,pc2_RI[1]))		
 	return(X_nonmg,pc2_RI, equation_parameters, z_transform, pc_transform)
 
-def evaluate_interval(nonmg: pd.DataFrame, mg: pd.DataFrame, lb: float, ub: float):
+def evaluate_interval(nonmg: pd.DataFrame, mg: pd.DataFrame, lb: float, ub: float)->None:
 	"""Evaluate PC2-based interval for MG diagnosis"""
 	X_mg=df2array(mg)
 	X_nonmg, pc2_RI, _, z_transform, pc_transform = derive_interval(nonmg,lb,ub)
 	performances=[]
-	performances.append(pd.DataFrame.from_dict(evaluate.SeSp_sFLCR(X_nonmg,X_mg,0.26,1.65),orient='index'))
-	performances.append(pd.DataFrame.from_dict(evaluate.SeSp_PCA(X_nonmg, X_mg, pc2_RI, pc_transform, z_transform),orient='index'))
+	performances.append(pd.DataFrame.from_dict(evaluate.SeSp_sFLCR(X_nonmg,X_mg,0.26,1.65),orient='index')) #evaluate manufacturer's sFLC-ratio based interval
+	performances.append(pd.DataFrame.from_dict(evaluate.SeSp_PCA(X_nonmg, X_mg, pc2_RI, pc_transform, z_transform),orient='index')) #evaluate pc2 interval (aka pc clonality index)
 	performance=pd.concat(performances,axis=0)
 	with open('./Output/performance.txt','w') as outfile:
 		performance.to_string(outfile)
@@ -112,29 +122,29 @@ def apply_interval(nonmg: pd.DataFrame,cases: pd.DataFrame, lb: float, ub: float
 	L_cases = transforms.log_transform(X_cases) #apply log transform to X_cases
 	Z_cases = z_transform(L_cases) #apply z transform to L_cases
 	cases['pc2'] = pc_transform(Z_cases)[:,1] #pc2 projections for nonmg cohort
-	cases['abnormal?'] = ((cases['pc2'] > pc2_RI[0]) & (cases['pc2'] < pc2_RI[1])) #add abnormality flag per PC2-based interval
+	cases['abnormal?'] = ((cases['pc2'] > pc2_RI[0]) & (cases['pc2'] < pc2_RI[1])) #add abnormality column per PC2-based interval
 	cases.to_csv('./Output/annotated_cases.csv')
 	visualize.plot_sflc(X_nonmg,pc2_RI, z_transform, pc_transform, X_cases=X_cases)
 
-
-
-#%%
-def main(nonmg_fpath=None,mg_fpath=None,cases_fpath=None,lb=0.25,ub=97.5)->None:
+def main(nonmg_fpath: str = None,mg_fpath: str = None,cases_fpath: str = None,lb: float = 2.5,ub: float = 97.5)->None:
 	"""Generates output per user specified options"""
 	if nonmg_fpath:
 		nonmg = pd.read_csv(options.nonmg_fpath)
 	else:
 		with open('./Data/WashU.p', 'rb') as file:
 			nonmg=pickle.load(file)
-			   
+
+	#generate and evaluate pc2-based interval			   
 	if mg_fpath:
 		mg = pd.read_csv(options.mg_fpath)
 		evaluate_interval(nonmg,mg,lb,ub)
 
+	#generate and apply pc2-based interval
 	if cases_fpath:
 		cases = pd.read_csv(options.cases_fpath)
 		apply_interval(nonmg,cases,lb,ub)
 
+	#only generate pc2-based interval and equation
 	if mg_fpath==None and cases_fpath==None:
 		derive_interval(nonmg,lb,ub)
 
@@ -157,4 +167,3 @@ if __name__ == '__main__':
 		lb=lb,
 		ub=ub
 	)
-# %%
