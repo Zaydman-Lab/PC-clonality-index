17062d1928b7736b22a7c240edebc8c173da4978
MarkZaydman <84101694+MarkZaydman@users.noreply.github.com>
2022-11-29 20:39:13 -0600

    Correct keys in parameter dictionary

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def case_1(nonmg: pd.DataFrame,lb: float,ub: float)->Tuple[pd.DataFrame]:
 		'B': np.mean(log_transform(X_normal)[:,0]), #Mean of log transformed kappa values for non_mg cohort
 		'C': np.std(log_transform(X_normal)[:,0]), #Standard deviation of log transformed kappa values for non_mg cohort
 		'D': Vh_raw[0,1], #Second right singular vector in raw space                      
-		'D': np.mean(log_transform(X_normal)[:,1]), #Mean of log transformed lambda values for non_mg cohort
+		'E': np.mean(log_transform(X_normal)[:,1]), #Mean of log transformed lambda values for non_mg cohort
 		'F': np.std(log_transform(X_normal)[:,1])} #Standard deviation of log transformed lambda values for non_mg cohort
 	visualize.plot_case1(X_nonmg, pc2_RI, z_transform, pc_transform)
 	return(X_nonmg,pc2_RI, equation_parameters, z_transform, pc_transform)
